3a832eae2d2fec9256f17d2b7e4613776957b34a
MidCoard <2624646185@qq.com>
2022-11-02 11:42:22 +0800

    modify README.md add example SN74HC595

---
+++ Core/Inc/example/SN74HC595.h
@@ -0,0 +1,52 @@
+//
+// Created by SimilarTriangles on 2022/10/24.
+//
+
+#ifndef R2R_TEST_74HC595_H
+#define R2R_TEST_74HC595_H
+
+#endif //R2R_TEST_74HC595_H
+
+#include "gpio.h"
+#include "tsms_driver.h"
+#include "tsms_gpio.h"
+
+#define SN74HC595_Q0 0
+#define SN74HC595_Q1 1
+#define SN74HC595_Q2 2
+#define SN74HC595_Q3 3
+#define SN74HC595_Q4 4
+#define SN74HC595_Q5 5
+#define SN74HC595_Q6 6
+#define SN74HC595_Q7 7
+
+#define SN74HC595_SQ0 0
+#define SN74HC595_SQ1 1
+#define SN74HC595_SQ2 2
+#define SN74HC595_SQ3 3
+#define SN74HC595_SQ4 4
+#define SN74HC595_SQ5 5
+#define SN74HC595_SQ6 6
+#define SN74HC595_SQ7 7
+
+#define SN74HC595_REG_STORAGE 0
+#define SN74HC595_REG_SHIFT 1
+
+#define SN74HC595_SRCLR 0
+#define SN74HC595_SRCLK 1
+#define SN74HC595_RCLK 2
+#define SN74HC595_DATA 3
+
+struct SN74HC595_Handler {
+	TSMS_DHP handler;
+};
+
+struct SN74HC595_Handler * SN74HC595_initHardware(GPIO_TypeDef* srclr, uint16_t srclrPin,
+                            GPIO_TypeDef* srclk, uint16_t srclkPin,
+                            GPIO_TypeDef* rclk, uint16_t rclkPin,
+                            GPIO_TypeDef* data, uint16_t dataPin);
+
+void SN74HC595_clearStorageRegister(struct SN74HC595_Handler* handler);
+void SN74HC595_writeByteToShiftRegister(struct SN74HC595_Handler* handler, uint8_t data);
+void SN74HC595_latchToStorageRegister(struct SN74HC595_Handler* handler);
+void SN74HC595_writeByte(struct SN74HC595_Handler* handler, uint8_t data);
--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 ## Description
 
-This library is still in working.
+This library is typically made for STM32 series MCU. Also, the TI MSP series are on the roadmap.
+
+This library contains following parts:
+
+- SPI driver (Both hardware and software SPI, only support master mode)
+- IIC driver (Both hardware and software IIC, only support master mode)
+- Custom digital interface (parallel-write, parallel-read, etc.)
+- Printer driver (Used to connect UART, Bluetooth, etc.)
+- GPIO driver (Interrupted is supported)
+- Utility functions (list, etc.)
+
+This library contains following examples:
+
+- SN74HC595 8-bit shift register
+- AD7606 16-bit ADC
