9b8f57b4293f13e653d35be8b8253e3b79991aca
MidCoard <2624646185@qq.com>
2023-03-13 19:54:22 +0800

    add tsms math lib

---
+++ Core/Inc/tsms_math.h
@@ -0,0 +1,8 @@
+#ifndef TSMS_MATH_H
+#define TSMS_MATH_H
+
+#include "tsms.h"
+
+TSMS_LSIZE min(TSMS_LSIZE a, TSMS_LSIZE b);
+
+#endif //TSMS_MATH_H
---
+++ Core/Src/tsms_math.c
@@ -0,0 +1,5 @@
+#include "tsms_math.h"
+
+TSMS_LSIZE min(TSMS_LSIZE a, TSMS_LSIZE b) {
+	return a < b ? a : b;
+}
