334a294056fd645acb54e28d6f0dff9a0595b890
MidCoard <2624646185@qq.com>
2023-03-17 09:18:19 +0800

    TSMS_UTIL_streamAppend steam could be NULL

--- Core/Src/tsms_util.c
+++ Core/Src/tsms_util.c
@@ -15,7 +15,7 @@ uint32_t TSMS_UTIL_reverseData(uint32_t v, uint8_t bits) {
 }
 
 uint8_t *TSMS_UTIL_streamAppend(uint8_t * stream, TSMS_SIZE size, uint8_t * data, TSMS_SIZE dataSize) {
-	if (stream == TSMS_NULL || data == TSMS_NULL)
+	if (data == TSMS_NULL)
 		return TSMS_NULL;
 	uint8_t * tmp = realloc(stream, size + dataSize);
 	if (tmp == TSMS_NULL)
