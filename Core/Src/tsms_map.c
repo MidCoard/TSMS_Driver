76a5b58efa1b2589dc65ecb487164141d0d02901
MidCoard <2624646185@qq.com>
2023-03-29 15:33:43 +0800

    remove tsms_string_map because of map has this feature

--- Core/Src/tsms_map.c
+++ Core/Src/tsms_map.c
@@ -1,18 +1,13 @@
 #include "tsms_map.h"
 #include "tsms_long_map.h"
-#include "tsms_string_map.h"
 
 TSMS_ME TSMS_EMPTY_MAP_ENTRY = {TSMS_NULL, TSMS_NULL};
 
 TSMS_LME TSMS_EMPTY_LONG_MAP_ENTRY = {0, TSMS_NULL};
 
-TSMS_SME TSMS_EMPTY_STRING_MAP_ENTRY = {TSMS_NULL, TSMS_NULL};
-
 TSMS_MI TSMS_EMPTY_MAP_ITERATOR = {TSMS_NULL, 0, TSMS_NULL};
 
 TSMS_LMI TSMS_EMPTY_LONG_MAP_ITERATOR = {TSMS_NULL, 0, TSMS_NULL};
-
-TSMS_SMI TSMS_EMPTY_STRING_MAP_ITERATOR = {TSMS_NULL, 0, TSMS_NULL};
 
 TSMS_INLINE TSMS_MNP __internal_tsms_create_node(void * key, void * value) {
 	TSMS_MNP node = malloc(sizeof (struct TSMS_MAP_NODE));
@@ -23,13 +18,6 @@ TSMS_INLINE TSMS_MNP __internal_tsms_create_node(void * key, void * value) {
 
 TSMS_INLINE TSMS_LMNP __internal_tsms_create_long_node(long key, void * value) {
 	TSMS_LMNP node = malloc(sizeof(struct TSMS_LONG_MAP_NODE));
-	node->key = key;
-	node->value = value;
-	return node;
-}
-
-TSMS_INLINE TSMS_SMNP __internal_tsms_create_string_node(pString key, void * value) {
-	TSMS_SMNP node = malloc(sizeof(struct TSMS_STRING_MAP_NODE));
 	node->key = key;
 	node->value = value;
 	return node;
@@ -361,170 +349,6 @@ TSMS_RESULT TSMS_LONG_MAP_clear(TSMS_LMP map) {
 	if (map == TSMS_NULL)
 		return TSMS_ERROR;
 	TSMS_LMNP cur, tmp;
-	for (TSMS_POS i = 0; i < map->diffusion; i++) {
-		cur = map->base[i];
-		while (cur != TSMS_NULL) {
-			tmp = cur;
-			cur = cur->next;
-			free(tmp);
-		}
-		map->base[i] = TSMS_NULL;
-	}
-	map->size = 0;
-	return TSMS_SUCCESS;
-}
-
-
-
-TSMS_SMP TSMS_STRING_MAP_create(TSMS_SIZE diffusion) {
-	TSMS_SMP map = malloc(sizeof(struct TSMS_STRING_MAP));
-	if (map == TSMS_NULL) {
-		tString temp = TSMS_STRING_temp("malloc failed for map");
-		TSMS_ERR_report(TSMS_ERR_MALLOC_FAILED, &temp);
-		return TSMS_NULL;
-	}
-	map->base = malloc(sizeof( TSMS_SMNP) * diffusion);
-	map->diffusion = diffusion;
-	map->size = 0;
-	return map;
-}
-
-TSMS_RESULT TSMS_STRING_MAP_put(TSMS_SMP map, pString key, void * value) {
-	if (map == TSMS_NULL)
-		return TSMS_ERROR;
-	TSMS_POS offset = TSMS_STRING_hash(key);
-	TSMS_SMNP cur = map->base[offset];
-	// first if not exist
-	if (cur == TSMS_NULL)
-		map->base[offset] = __internal_tsms_create_string_node(key, value);
-	else {
-		// if existed, judge it!
-		if (cur->key == key) {
-			if (cur->value == value)
-				return TSMS_FAIL;
-			cur->value = value;
-			return TSMS_SUCCESS;
-		}
-
-		while (cur->next != TSMS_NULL) {
-			if (cur->next->key == key) {
-				if (cur->next->value == value)
-					return TSMS_FAIL;
-				cur->next->value = value;
-				return TSMS_SUCCESS;
-			} else cur = cur->next;
-		}
-
-		if (cur->next == TSMS_NULL)
-			cur->next = __internal_tsms_create_string_node(key, value);
-	}
-	map->size++;
-	return TSMS_SUCCESS;
-}
-
-void* TSMS_STRING_MAP_get(TSMS_SMP map, pString key) {
-	if (map == TSMS_NULL)
-		return TSMS_NULL;
-	TSMS_POS offset = TSMS_STRING_hash(key);
-	TSMS_SMNP cur = map->base[offset];
-	while (cur != TSMS_NULL) {
-		if (cur->key == key)
-			return cur->value;
-		cur = cur->next;
-	}
-	return TSMS_NULL;
-}
-
-TSMS_RESULT TSMS_STRING_MAP_remove(TSMS_SMP map, pString key) {
-	if (map == TSMS_NULL)
-		return TSMS_ERROR;
-	TSMS_POS offset = TSMS_STRING_hash(key);
-	TSMS_SMNP cur = map->base[offset];
-	if (cur == TSMS_NULL)
-		return TSMS_FAIL;
-	if (cur->key == key) {
-		map->base[offset] = cur->next;
-		free(cur);
-		map->size--;
-		return TSMS_SUCCESS;
-	}
-	while (cur->next != TSMS_NULL) {
-		if (cur->next->key == key) {
-			TSMS_SMNP tmp = cur->next;
-			cur->next = cur->next->next;
-			free(tmp);
-			map->size--;
-			return TSMS_SUCCESS;
-		} else cur = cur->next;
-	}
-	return TSMS_FAIL;
-}
-
-TSMS_RESULT TSMS_STRING_MAP_release(TSMS_SMP map) {
-	if (map == TSMS_NULL)
-		return TSMS_ERROR;
-	TSMS_SMNP cur, tmp;
-	for (TSMS_POS i = 0; i < map->diffusion; i++) {
-		cur = map->base[i];
-		while (cur != TSMS_NULL) {
-			tmp = cur;
-			cur = cur->next;
-			free(tmp);
-		}
-	}
-	free(map->base);
-	free(map);
-	return TSMS_SUCCESS;
-}
-
-TSMS_SMI TSMS_STRING_MAP_iterator(TSMS_SMP map) {
-	if (map == TSMS_NULL)
-		return TSMS_EMPTY_STRING_MAP_ITERATOR;
-	TSMS_SMI iter = {map, 0, map->base[0]};
-	return iter;
-}
-
-bool TSMS_STRING_MAP_hasNext(TSMS_SMIP iter) {
-	if (iter == TSMS_NULL || iter == &TSMS_EMPTY_STRING_MAP_ITERATOR)
-		return false;
-	if (iter->next == TSMS_NULL) {
-		TSMS_POS i = iter->current + 1;
-		for (; i < iter->map->diffusion; i++)
-			if (iter->map->base[i] != TSMS_NULL)
-				return true;
-		return false;
-	}
-	return true;
-}
-
-TSMS_SME TSMS_STRING_MAP_next(TSMS_SMIP iter) {
-	if (iter == TSMS_NULL || iter == &TSMS_EMPTY_STRING_MAP_ITERATOR)
-		return TSMS_EMPTY_STRING_MAP_ENTRY;
-	if (iter->next == TSMS_NULL) {
-		TSMS_POS i = iter->current + 1;
-		for (; i < iter->map->diffusion; i++)
-			if (iter->map->base[i] != TSMS_NULL) {
-				iter->current = i;
-				iter->next = iter->map->base[i];
-				TSMS_SME entry;
-				entry.key = iter->next->key;
-				entry.value = iter->next->value;
-				iter->next = iter->next->next;
-				return entry;
-			}
-		return TSMS_EMPTY_STRING_MAP_ENTRY;
-	}
-	TSMS_SME entry;
-	entry.key = iter->next->key;
-	entry.value = iter->next->value;
-	iter->next = iter->next->next;
-	return entry;
-}
-
-TSMS_RESULT TSMS_STRING_MAP_clear(TSMS_SMP map) {
-	if (map == TSMS_NULL)
-		return TSMS_ERROR;
-	TSMS_SMNP cur, tmp;
 	for (TSMS_POS i = 0; i < map->diffusion; i++) {
 		cur = map->base[i];
 		while (cur != TSMS_NULL) {
