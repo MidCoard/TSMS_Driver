9b20460a4d44b32d2a7170333b45411f17f5fa71
MidCoard <2624646185@qq.com>
2023-04-17 10:59:06 +0800

    rearrange files

--- Core/Src/tsms_err.c
+++ Core/Src/tsms_err.c
@@ -1,9 +1,7 @@
 #include "tsms_err.h"
 
-#ifdef TSMS_ERROR_REPORT
-
 void TSMS_ERR_report(TSMS_ERR_TYPE type, pString message) {
+#ifdef TSMS_ERROR_REPORT
 	printf("TSMS ERROR: %d, %s\n", type, message->cStr);
-}
-
 #endif
+}
